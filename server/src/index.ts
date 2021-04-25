8d3374165710b79f019a7c960530a56b3675eb78
Dave Cranwell <xcession@xcessionsites.com>
2021-04-25 23:07:14 +0100

    Employs state machine

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,39 +1,46 @@
 import config from 'config'
 import { Server as SocketServer } from 'socket.io'
+import { Event, AnyEventObject } from 'xstate'
 
 import app from './app'
 import makeLogger from './logger'
 import makeSerialConnection from './lib/serialInterface'
-import lookupNumber from './lib/lookupNumber'
+
+import makeStateMachine from './stateMachine'
 
 const logger = makeLogger('index')
-const port = config.get('port')
 
+const port = config.get('port')
 const express = app.listen(port)
 
-const io = new SocketServer(express, {
-  cors: { origin: 'http://localhost:3000' },
-}).on('connection', (socket) => {
+const io = new SocketServer(express)
+const stateMachine = makeStateMachine(io)
+
+io.on('connection', (socket) => {
   logger.info('Client socket connected')
+  console.log(stateMachine.state)
+  io.emit('currentstate', stateMachine.state)
 })
+
+interface StateMachineEventData extends AnyEventObject {
+  type: string
+  val?: string
+}
 
 express.on('listening', async () => {
   logger.info('Started server')
 
   const serialPort = makeSerialConnection({
     onData: async (data: string) => {
-      if (data.indexOf('NMBR=') > -1) {
-        const number: string = data.split('=').pop()?.trim() as string
+      // format serial data nicer
+      const stateData: StateMachineEventData = data.includes('=')
+        ? {
+            type: data.split('=').shift()!.trim(),
+            val: data.split('=').pop()?.trim(),
+          }
+        : { type: data.trim() }
 
-        //const rating = await lookupNumber(number);
-
-        io.emit('/caller', {
-          number,
-          rating: 'dangerous',
-        })
-      }
-
-      return data
+      stateMachine.send(stateData)
     },
   })
 
