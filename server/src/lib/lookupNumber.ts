7f1ee583c3fcb1955119d5649f4d2741f892a9d9
Dave Cranwell <xcession@xcessionsites.com>
2021-04-25 23:08:17 +0100

    Comments out who-called

--- server/src/lib/lookupNumber.ts
+++ server/src/lib/lookupNumber.ts
@@ -3,9 +3,14 @@ import cheerio from 'cheerio'
 
 export default async (number: string) => {
   try {
-    const { data } = await axios.get(
-      `https://who-called.co.uk/Number/${number}`
-    )
+    // const { data } = await axios.get(
+    //   `https://who-called.co.uk/Number/${number}`
+    // )
+    const data = `<html>
+      <div class="numberDetails">
+        <div class="dataColumn">Dangerous</div>
+      </div>
+    </html>`
 
     const $ = cheerio.load(data)
     const rating = $('.numberDetails .dataColumn').text()
