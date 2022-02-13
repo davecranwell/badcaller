e20857c41ec598ae2821d9e9f53ca9198f01a423
Dave Cranwell <xcession@xcessionsites.com>
2022-02-13 12:45:32 +0000

    Instruction order fix

--- makedocker.sh
+++ makedocker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-DOCKER_BUILDKIT=1 docker build . -t xcession2k/badcaller:latest
 cd server && npm version minor && cd ..
 cd client && npm version minor && cd ..
+DOCKER_BUILDKIT=1 docker build . -t xcession2k/badcaller:latest
 docker login
 docker push xcession2k/badcaller:latest
