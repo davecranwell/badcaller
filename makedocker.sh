df400d7d31c77d0e3fa0a7484568cc2ff589f599
Dave Cranwell <xcession@xcessionsites.com>
2022-09-22 21:01:12 +0100

    adds skafold and fixes builds that fail

--- makedocker.sh
+++ makedocker.sh
@@ -2,6 +2,13 @@
 
 cd server && npm version minor && cd ..
 cd client && npm version minor && cd ..
+
 DOCKER_BUILDKIT=1 docker build . -t xcession2k/badcaller:latest
-docker login
-docker push xcession2k/badcaller:latest
+
+if [ $? -eq 0 ]; then
+  docker login
+  docker push xcession2k/badcaller:latest
+else
+  echo FAIL
+fi
+
